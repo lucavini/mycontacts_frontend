b9e17f22bc2f356136d65d1a74330106de02f1c5
Lucas Santos <lucas.santos@mblabs.com.br>
2023-02-13 20:15:19 -0300

    feat: ContactsList component

---
+++ src/Components/ContactsList/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Header } from './styles';
+
+function index() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+    </Container>
+  );
+}
+
+export default index;
