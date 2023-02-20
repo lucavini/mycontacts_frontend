88893967abf2c700693edadbb1dfc7822f1050e2
lucas santos <lucas.santos@mblabs.com.br>
2023-02-20 20:24:24 -0300

    feat: search contacts with useMemo

--- src/pages/Home/controller.ts
+++ src/pages/Home/controller.ts
@@ -10,14 +10,21 @@ interface Contact {
 }
 
 export interface Controller {
-  contacts: Contact[];
+  filteredContacts: Contact[];
   orderBy: string;
+  searchTerm: string;
   handleToggleOrderBy: () => void;
+  handleChangeSearchTerm: (value: string) => void;
 }
 
 export function controller(): Controller {
   const [contacts, setContacts] = React.useState<Contact[]>([]);
   const [orderBy, setOrderBy] = React.useState('asc');
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const filteredContacts = React.useMemo(() => contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )), [contacts, searchTerm]);
 
   React.useEffect(() => {
     fetch(`http://localhost:3333/contacts?orderBy=${orderBy}`)
@@ -34,5 +41,15 @@ export function controller(): Controller {
     setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
   }
 
-  return { contacts, orderBy, handleToggleOrderBy };
+  function handleChangeSearchTerm(value: string) {
+    setSearchTerm(value);
+  }
+
+  return {
+    orderBy,
+    searchTerm,
+    filteredContacts,
+    handleToggleOrderBy,
+    handleChangeSearchTerm,
+  };
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,30 +14,43 @@ import edit from '../../assets/icons/edit.svg';
 import trash from '../../assets/icons/delete.svg';
 
 function Home() {
-  const { contacts, orderBy, handleToggleOrderBy } = controller();
+  const {
+    orderBy,
+    searchTerm,
+    filteredContacts,
+    handleToggleOrderBy,
+    handleChangeSearchTerm,
+  } = controller();
 
   return (
     <Container>
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato" />
+        <input
+          type="text"
+          placeholder="Pesquisar contato"
+          value={searchTerm}
+          onChange={({ target }) => handleChangeSearchTerm(target.value)}
+        />
       </InputSearchContainer>
 
       <Header>
         <strong>
-          {contacts.length}
-          {contacts.length === 1 ? ' Contato' : ' Contatos'}
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' Contato' : ' Contatos'}
         </strong>
         <Link to="/new">Novo Contato</Link>
       </Header>
 
-      <ListHeader orderBy={orderBy}>
-        <button type="button" onClick={handleToggleOrderBy}>
-          <span>Nome</span>
-          <img src={arrow} alt="arrow" />
-        </button>
-      </ListHeader>
+      {!!filteredContacts.length && (
+        <ListHeader orderBy={orderBy}>
+          <button type="button" onClick={handleToggleOrderBy}>
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </ListHeader>
+      )}
 
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
