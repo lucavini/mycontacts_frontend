31919059fb16d3a1633b769f5238150bfd7adc3f
Lucas Santos <lucas.santos@mblabs.com.br>
2023-04-09 10:36:51 -0300

    feat: service modules

---
+++ src/Shared/Services/utils/HttpClient.ts
@@ -0,0 +1,28 @@
+import APIError from '../../Errors/APIError';
+import delay from '../../../utils/delay';
+
+export default class HttpClient {
+  baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  async get(path: string) {
+    await delay();
+
+    const response = await fetch(`${this.baseUrl}${path}`);
+
+    let body = null;
+    const contentType = response.headers.get('Content-Type');
+    if (contentType?.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new APIError(response, body);
+  }
+}
