013bec8e30f60ed0a77e9ace9dc4b12386f958ad
Lucas Santos <lucas.santos@mblabs.com.br>
2023-04-09 10:36:32 -0300

    feat: global modules

---
+++ src/Shared/models/module.d.ts
@@ -0,0 +1,19 @@
+export as namespace models;
+
+export interface Contact {
+  category_id: string;
+  category_name: string;
+  email: string;
+  id: string;
+  name: string;
+  phone: string;
+}
+
+export interface Category {
+  category_id: string;
+  category_name: string;
+  email: string;
+  id: string;
+  name: string;
+  phone: string;
+}
