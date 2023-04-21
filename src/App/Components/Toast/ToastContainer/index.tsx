d4265cf7afeac389300c625b84ea7e5993db6ca7
Lucas Santos <lucas.santos@mblabs.com.br>
2023-04-21 10:39:11 -0300

    feat: event listener to handler toast events

--- src/App/Components/Toast/ToastContainer/index.tsx
+++ src/App/Components/Toast/ToastContainer/index.tsx
@@ -3,12 +3,42 @@ import React from 'react';
 import { Container } from './styles';
 import ToastMessage from '../ToastMessage';
 
+interface Message {
+  id: number;
+  type: toast.ToastTypes['type'];
+  text: string;
+}
+
 function ToastContainer() {
+  const [messages, setMessages] = React.useState<Message[]>([]);
+
+  React.useEffect(() => {
+    function handleAddToats(event: Event) {
+      const customEvent = event as CustomEvent<toast.ToastDetails>;
+      const { text, type } = customEvent.detail;
+
+      setMessages((prevState) => [
+        ...prevState,
+        { id: Math.random(), text, type },
+      ]);
+    }
+
+    document.addEventListener('addtoast', handleAddToats);
+
+    return () => {
+      document.removeEventListener('addtoast', handleAddToats);
+    };
+  }, []);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" type="default" />
-      <ToastMessage text="Error toast" type="error" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          text={message.text}
+          type={message.type}
+        />
+      ))}
     </Container>
   );
 }
